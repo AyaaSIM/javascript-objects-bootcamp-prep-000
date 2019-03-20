6f48dafbc56446cc8889fdf7168472dccd0de4eb
Ayaa <ayaa1siddig5@gmail.com>
2019-03-20 10:42:48 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,9 +1,1 @@
-var playlist = { JustinBieber : "Baby"};
-function updatePlaylist (playlist, artistName, songtitle){
- playlist[artistName] = songtitle
-  return playlist
-}
-function removeFromPlaylist(playlist, artistName){
-  delete playlist[artistName];
-  return playlist
-}
+var playlist = { Justin Bieber: Baby
