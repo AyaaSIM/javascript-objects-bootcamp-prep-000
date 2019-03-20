bb70eea01df50e00f09a5478102e98b526945927
Ayaa <ayaa1siddig5@gmail.com>
2019-03-20 11:04:19 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
   return updatePlaylist
 function removeFromPlaylist(playlist, artistName)
-delete playlist.[artistName]
+delete artistName
 return removeFromPlaylist
 }
 
